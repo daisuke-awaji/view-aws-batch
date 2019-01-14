a5237a91a9cee561e5cd82abb374219d251b1434
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2019-01-15 06:39:27 +0900

    filterの実装

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AWS from 'aws-sdk';
+
+AWS.config.update({
+  "accessKeyId": process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  "secretAccessKey": process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  "region": process.env.REACT_APP_AWS_DEFAULT_REGION
+})
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: this.props.jobs.jobSummaryList,
+    }
+  }
+
+  filterList = (e) => {
+    const updateList = this.props.jobs.jobSummaryList.filter((item) => {
+      return this.matchStringValue(item.jobId, e.target.value)
+      || this.matchStringValue(item.jobName, e.target.value);
+    })
+    this.setState({jobs: updateList});
+  }
+
+  // strが targetと一致すればtrueを返却
+  matchStringValue(str, target) {
+    return str.toLowerCase().search( target.toLowerCase() ) !== -1;
+  }
+
+  componentDidMount() {
+  }
+
   render() {
+    let list = this.state.jobs.map((job) => (
+      <li key={job.jobId}>
+        {job.jobId} {job.jobName} {job.createdAt} {job.status} {job.statusReason} {job.startedAt} {job.stoppedAt}
+      </li>
+    ));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form action="">
+          <input
+            type="text"
+            ref="myinput"
+            defaultValue=""
+            onChange={this.filterList}
+          />
+        </form>
+        <ul>
+          {list}
+        </ul>
       </div>
     );
   }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,53 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const jobs = {
+ jobSummaryList: [
+   {
+     jobId: "e66ff5fd-a1ff-4640-b1a2-0b0a142f49bb",
+     jobName: "example",
+     createdAt: "2019/11/11 10:32:22",
+     status: "RUNNING",
+     statusReason: "hey you",
+     startedAt: "4567898763456898",
+     stoppedAt: "3200832858",
+     // container: {
+     //   exitCode: "1",
+     //   reason: "yeah"
+     // },
+   },
+   {
+     jobId: "v0oijtwea0uiogfjoifupsb0dbs98bdfslj",
+     jobName: "yeah",
+     createdAt: "2019/02/11 09:32:22",
+     status: "PENDING",
+     statusReason: "waiting...",
+     startedAt: "09vdsoj;lkadjsa",
+     stoppedAt: "3456789876545678",
+     // container: {
+     //   exitCode: "1",
+     //   reason: "yeah"
+     // },
+   },
+   {
+     jobId: "g8ds09goqw4tj0v90slt32ggsagdklajf;ldsa",
+     jobName: "yeah job",
+     createdAt: "2011/02/27 09:32:22",
+     status: "SUCCEEDED",
+     statusReason: "successful done",
+     startedAt: "09328502590",
+     stoppedAt: "0490952935092",
+     // container: {
+     //   exitCode: "1",
+     //   reason: "yeah"
+     // },
+   },
+ ],
+ nextToken: "90ujb9908fd7s0jhbsdf0"
+}
+
+ReactDOM.render(<App jobs={jobs}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
